e3f253b1873dcb8e61bf9d4609115e60fb25d033
Toan Nguyen <tnguyen@us.maxnerva.com>
2024-08-31 18:23:12 +0700

    update flyway init table

---
+++ src/main/resources/db/migration/mysql/V0__outbox_events.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS outboxevent (
+    id             BINARY(16) NOT NULL PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+    timestamp      TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    aggregate_type VARCHAR(100) NOT NULL,
+    aggregate_id   VARCHAR(100) NOT NULL,
+    type           VARCHAR(100) NOT NULL,
+    payload        JSON        NOT NULL
+);
